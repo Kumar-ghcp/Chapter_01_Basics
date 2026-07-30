60e2dd5064bc8966b481204527fd25e75db92c65
Kumar <Nestle.choc@gmail.com>
2026-07-30 11:14:10 +0530

    added new files

--- Chapter_01_Basics/tests/dist/For_loop_Array.dev.js
+++ Chapter_01_Basics/tests/dist/For_loop_Array.dev.js
@@ -9,5 +9,23 @@
 // for (let i = 0; i < chars.length; i++) {
 //     console.log(chars[i], i);
 // }
-var browsers = ["chrome", "firefox", "safari", "edge", "opera"];
-console.log(browsers);
+//let browsers = ["chrome", "firefox", "safari", "edge", "opera"];
+// console.log(browsers[browsers.length - 2]);
+// let numbers = new Array(100, 200, 300, 400, 500);
+// console.log(numbers[4]);
+// let numbers2 = new Array(5);
+// console.log(numbers2[4]);
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+arr.push(6);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.push(7);
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift(1);
+console.log(arr);
+arr.splice(5, 1, 6, 11);
+console.log(arr.join(", "));
